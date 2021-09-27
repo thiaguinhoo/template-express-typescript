dcc674dc354a7c2a3a4ade79581b87acc9015289
thiaguinhoo <thiagoluizsog@gmail.com>
2021-09-27 09:18:51 -0300

    change src/application.ts

--- src/application.ts
+++ src/application.ts
@@ -2,13 +2,13 @@ import express from 'express';
 import compression from 'compression';
 import helmet from 'helmet';
 import cors from 'cors';
-import morgan from 'morgan'
+import morgan from 'morgan';
 
 const application = express();
 
 application.use(express.json());
 application.use(express.urlencoded({ extended: false }));
-application.use(helmet())
+application.use(helmet());
 application.use(cors());
 application.use(compression());
 if (process.env.NODE_ENV === 'development') {
@@ -16,4 +16,3 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 export default application;
-
